7bca189a9f0b82d9395a1d3d68014f18ac9de0d1
Aman Malik <amanmalik0135@gmail.com>
2025-05-01 15:31:56 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,22 @@
-
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
-import GreetingCompontes from "./greeting";
 
 const App = () => {
+  const [input,setInput] = useState('');
+  const onInput = (e)=>{
+      const {value} = e.target
+    setInput(value)
+  }
+  const onClear = ()=>{
+    setInput('')
+  }
+  
   return (
     <div>
-      <GreetingCompontes/>
+      <p>Enter your name:</p>
+    <input value={input} onChange={onInput} />
+    {!!input.length&&<p>Hello {input}!</p>}
+    <button onClick={onClear}>Clear</button>
     </div>
   )
 }
